955613dbe0da622c3812e22d7b1d11459c3c60df
adustman <adustman@9fc6cd9a-920d-0410-adcf-ac96716ed7e8>
2001-09-07 01:06:08 +0000

    Version 0.9.1c1

--- MySQLdb/setup.py
+++ MySQLdb/setup.py
@@ -75,7 +75,7 @@ MySQLdb. MySQLdb is free software.
 
 setup (# Distribution meta-data
         name = "MySQL-python",
-        version = "0.9.1",
+        version = "0.9.1c1",
         description = "An interface to MySQL",
 	long_description=long_description,
         author = "Andy Dustman",
