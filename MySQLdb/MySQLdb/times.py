03a0fcdd1f70687e66c83e060ea43481a4593053
adustman <adustman@9fc6cd9a-920d-0410-adcf-ac96716ed7e8>
2003-07-10 15:58:56 +0000

    Add support for Python 2.3 datetime classes.

--- MySQLdb/MySQLdb/times.py
+++ MySQLdb/MySQLdb/times.py
@@ -3,93 +3,18 @@
 This module provides some Date and Time classes for dealing with MySQL data.
 """
 
-from time import strftime, localtime
 from _mysql import string_literal
 
 try:
-    try:
-        # new packaging
-        from mx.DateTime import Date, Time, Timestamp, ISO, \
-            DateTimeType, DateTimeDeltaType
-    except ImportError:
-        # old packaging, deprecated
-        from DateTime import Date, Time, Timestamp, ISO, \
-            DateTimeType, DateTimeDeltaType
-
-    def DateFromTicks(ticks):
-        """Convert UNIX ticks into a mx.DateTime.Date."""
-	return apply(Date, localtime(ticks)[:3])
-
-    def TimeFromTicks(ticks):
-        """Convert UNIX ticks into a mx.DateTime.Time."""
-	return apply(Time, localtime(ticks)[3:6])
-
-    def TimestampFromTicks(ticks):
-        """Convert UNIX ticks into a mx.DateTime.Timestamp."""
-	return apply(Timestamp, localtime(ticks)[:6])
-
-    def format_DATE(d):
-        """Format a DateTime object as an ISO date."""
-        return d.strftime("%Y-%m-%d")
-    
-    def format_TIME(d):
-        """Format a DateTime object as a time value."""
-        return d.strftime("%H:%M:%S")
-    
-    def format_TIMESTAMP(d):
-        """Format a DateTime object as an ISO timestamp."""
-        return d.strftime("%Y-%m-%d %H:%M:%S")
-
-    def DateTime_or_None(s):
-        try: return ISO.ParseDateTime(s)
-        except: return None
-
-    def TimeDelta_or_None(s):
-        try: return ISO.ParseTimeDelta(s)
-        except: return None
-
-    def Date_or_None(s):
-        try: return ISO.ParseDate(s)
-        except: return None
-
+    from pytimes import *
 
 except ImportError:
-    # no DateTime? We'll muddle through somehow.
-    
-    DateTimeDeltaType = "DateTimeDeltaType"
-    DateTimeType = "DateTimeType"
-    
-    def Date(year, month, day):
-        """Construct an ISO date string."""
-        return "%04d-%02d-%02d" % (year, month, day)
+    try:
+        from mxdatetimes import *
 
-    def Time(hour, min, sec):
-        """Construct a TIME string."""
-        return "%02d:%02d:%02d" % (hour, min, sec)
-
-    def Timestamp(year, month, day, hour, min, sec):
-        """Construct an ISO timestamp."""
-        return "%04d-%02d-%02d %02d:%02d:%02d" % \
-               (year, month, day, hour, min, sec)
-    
-    def DateFromTicks(ticks):
-        """Convert UNIX ticks to ISO date format."""
-	return strftime("%Y-%m-%d", localtime(ticks))
-
-    def TimeFromTicks(ticks):
-        """Convert UNIX ticks to time format."""
-	return strftime("%H:%M:%S", localtime(ticks))
-
-    def TimestampFromTicks(ticks):
-        """Convert UNIX ticks to ISO timestamp format."""
-	return strftime("%Y-%m-%d %H:%M:%S", localtime(ticks))
-
-    def format_DATE(d):
-        """Format a date as a date (does nothing, you don't have mx.DateTime)."""
-        return d
-
-    format_TIME = format_TIMESTAMP = format_DATE
-    TimeDelta_or_None = Date_or_None = DateTime_or_None = format_DATE
+    except ImportError:
+        # no DateTime? We'll muddle through somehow.
+        from stringtimes import *
 
 def DateTime2literal(d, c):
     """Format a DateTime object as an ISO timestamp."""
