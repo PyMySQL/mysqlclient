a050932372d7b94e8606caa0f242dd2ff4b77cfe
Tres Seaver <tseaver@palladion.com>
2013-06-14 13:34:07 -0400

    If setuptools is importable, don't force use of 'distribute'.
    Allows use of post-merge setuptools 0.7.

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@
 import os
 import sys
 
-from distribute_setup import use_setuptools
-use_setuptools()
+try:
+    import setuptools
+except ImportError:
+    from distribute_setup import use_setuptools
+    use_setuptools()
 from setuptools import setup, Extension
 
 if not hasattr(sys, "hexversion") or sys.hexversion < 0x02040000:
