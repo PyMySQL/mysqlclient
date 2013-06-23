0d1dd6fa894d50ce9bec0f059d60321930ad5277
Monty Taylor <mordred@inaugust.com>
2013-06-23 18:16:17 -0400

    Remove distribute dependency.
    Distribute has been merged with upstream setuptools. The two are
    incompatible now, so MySQL-python is uninstallable on systems
    with setuptools >= 0.7 installed.

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@
 import os
 import sys
 
-from distribute_setup import use_setuptools
-use_setuptools()
 from setuptools import setup, Extension
 
 if not hasattr(sys, "hexversion") or sys.hexversion < 0x02040000:
