052955e093e8db7b1a2fdfd7707de39980f849cd
Dima Tyzhnenko <t.dmitry@gmail.com>
2013-10-07 12:34:14 +0300

    Fix cut off SQL query when its contained `))`, edit insert_values regexp. Fixes issue #33.

--- MySQLdb/cursors.py
+++ MySQLdb/cursors.py
@@ -26,7 +26,7 @@ restr = r"""
                 (?:
                         (?:\(
                             # ( - editor hightlighting helper
-                            [^)]*
+                            .*
                         \))
                     |
                         '
