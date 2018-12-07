e48e393e68112f88f3db575ef4a34b1c5ad08370
Inada Naoki <songofacandy@gmail.com>
2018-12-07 13:51:17 +0900

    Update compat

--- MySQLdb/compat.py
+++ MySQLdb/compat.py
@@ -1,12 +1,12 @@
 import sys
 
-if sys.version_info[0] == 3:
-    PY2 = False
-    unicode = str
-    unichr = chr
-    long = int
-else:
+if sys.version_info[0] == 2:
     PY2 = True
     unicode = unicode
     unichr = unichr
     long = long
+else:
+    PY2 = False
+    unicode = str
+    unichr = chr
+    long = int
