c61f5452335f11b8c1895acd810a82c5d76b2efe
Inada Naoki <songofacandy@gmail.com>
2023-05-09 05:04:25 +0000

    black

--- tests/dbapi20.py
+++ tests/dbapi20.py
@@ -525,8 +525,7 @@ class DatabaseAPI20Test(unittest.TestCase):
         tests.
         """
         populate = [
-            f"insert into {self.table_prefix}booze values ('{s}')"
-            for s in self.samples
+            f"insert into {self.table_prefix}booze values ('{s}')" for s in self.samples
         ]
         return populate
 
