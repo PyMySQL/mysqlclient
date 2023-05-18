5dfab4d9c5c5bc141eb66ea761efe7a7b51e1956
Inada Naoki <songofacandy@gmail.com>
2023-05-19 03:04:56 +0900

    CI: Update Django test workflow (#610)

--- ci/test_mysql.py
+++ ci/test_mysql.py
@@ -19,7 +19,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "USER": "scott",
         "PASSWORD": "tiger",
-        "TEST": {"CHARSET": "utf8mb4", "COLLATION": "utf8mb4_general_ci"},
+        "TEST": {"CHARSET": "utf8mb3", "COLLATION": "utf8mb3_general_ci"},
     },
     "other": {
         "ENGINE": "django.db.backends.mysql",
@@ -27,7 +27,7 @@ DATABASES = {
         "HOST": "127.0.0.1",
         "USER": "scott",
         "PASSWORD": "tiger",
-        "TEST": {"CHARSET": "utf8mb4", "COLLATION": "utf8mb4_general_ci"},
+        "TEST": {"CHARSET": "utf8mb3", "COLLATION": "utf8mb3_general_ci"},
     },
 }
 
@@ -37,3 +37,5 @@ SECRET_KEY = "django_tests_secret_key"
 PASSWORD_HASHERS = [
     "django.contrib.auth.hashers.MD5PasswordHasher",
 ]
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
