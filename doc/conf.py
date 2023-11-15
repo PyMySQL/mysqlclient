2e86ccec70b7863922d192afc3167718e4fb8714
Inada Naoki <songofacandy@gmail.com>
2023-11-15 19:14:04 +0900

    use sphinx-rtd-theme (#668)

--- doc/conf.py
+++ doc/conf.py
@@ -49,8 +49,8 @@ source_suffix = ".rst"
 master_doc = "index"
 
 # General information about the project.
-project = "MySQLdb"
-copyright = "2012, Andy Dustman"
+project = "mysqlclient"
+copyright = "2023, Inada Naoki"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -100,7 +100,7 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
